8de6f97c04cd179f2be3e4b7726d19b726946252
Shane <shanemorton@live.com.au>
2026-09-20 18:20:37 +1000

    attract slide images added and press start flashing sequence

---
+++ slides/label_2.gd
@@ -0,0 +1,14 @@
+
+extends Label
+
+func _ready():
+	visible = false
+	
+	#wait 8 seconds before first showing
+	await get_tree() .create_timer(8.0).timeout
+	visible = true
+	#then blink every 1 second
+	while true:
+		await get_tree() .create_timer(0.5).timeout
+		visible = !visible
+	
