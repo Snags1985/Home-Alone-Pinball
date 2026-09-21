dd64a0f57025a2aa3c6cb50ff127a6ef9a189b49
Shane <shanemorton@live.com.au>
2026-09-21 14:48:41 +1000

    adjusted timings of press start

--- slides/label_2.gd
+++ slides/label_2.gd
@@ -5,9 +5,9 @@ func _ready():
 	visible = false
 	
 	#wait 8 seconds before first showing
-	await get_tree() .create_timer(8.0).timeout
+	await get_tree() .create_timer(2.0).timeout
 	visible = true
-	#then blink every 1 second
+	#then blink every 0.3 second
 	while true:
 		await get_tree() .create_timer(0.5).timeout
 		visible = !visible
